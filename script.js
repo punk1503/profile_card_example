6d361ab869db312c165cbfcfad69d8cdf8592a20
punk1503 <zaharov.andrey161@gmail.com>
2021-02-23 00:36:03 +0300

    I want to die

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function like() {
-    alert('You liked this person');
-}
-
-function dislike() {
-    alert('You disliked this person');
+let code = false;
+function switch_card()
+{
+    console.log('clicked');
+    profile_img = document.getElementsByClassName('card-img')[0];
+    if(code)
+    {
+        profile_img.src = 'https://placekitten.com/400/450';
+    }
+    else
+    {
+        profile_img.src = "./carbon.png";
+    }
+    code = !code;
 }
